34910b3be9e8f2966a131a4a25a120565a8daf4d
arunsinghh <arunsinghh.dev@gmail.com>
2025-05-04 13:31:28 +0530

    first

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,15 +15,16 @@ function Navbar() {
             OrthoproIndia
           </Link>
 
-          {/* Hamburger Menu (Mobile) */}
+          {/* Hamburger (mobile) */}
           <div className="md:hidden">
             <button onClick={() => setMobileOpen(!mobileOpen)}>
               {mobileOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
             </button>
           </div>
 
-          {/* Desktop Menu */}
+          {/* Desktop Nav */}
           <div className="hidden md:flex items-center space-x-4">
+            {/* Services dropdown */}
             <div className="relative group inline-block">
               <button className="flex items-center space-x-1 px-4 py-2 hover:bg-blue-700 rounded-md">
                 <span>Services</span>
@@ -37,6 +38,7 @@ function Navbar() {
               </div>
             </div>
 
+            {/* More dropdown */}
             <div className="relative group inline-block">
               <button className="flex items-center space-x-1 px-4 py-2 hover:bg-blue-700 rounded-md">
                 <span>More</span>
@@ -50,30 +52,42 @@ function Navbar() {
           </div>
         </div>
 
-        {/* Mobile Menu */}
+        {/* Mobile Nav */}
         {mobileOpen && (
-          <div className="md:hidden mt-2 space-y-2 pb-4">
-            <button onClick={() => setServicesOpen(!servicesOpen)} className="flex justify-between w-full px-4 py-2 bg-blue-500">
-              Services <ChevronDown className="w-4 h-4" />
-            </button>
-            {servicesOpen && (
-              <div className="bg-blue-100 text-gray-800 space-y-1 px-6 py-2">
-                <Link to="/prosthetic-services" onClick={() => setMobileOpen(false)}>Prosthetic Services</Link>
-                <Link to="/orthotic-services" onClick={() => setMobileOpen(false)}>Orthotic Services</Link>
-                <Link to="/diabetic-care" onClick={() => setMobileOpen(false)}>Diabetic Foot & Wound Care</Link>
-                <Link to="/pediatric-care" onClick={() => setMobileOpen(false)}>Pediatric Care</Link>
-              </div>
-            )}
+          <div className="md:hidden mt-2 pb-4 space-y-2">
+            {/* Services Dropdown */}
+            <div>
+              <button
+                onClick={() => setServicesOpen(!servicesOpen)}
+                className="w-full flex justify-between items-center px-4 py-2 bg-blue-500"
+              >
+                Services <ChevronDown className="w-4 h-4" />
+              </button>
+              {servicesOpen && (
+                <div className="bg-blue-100 text-gray-800 space-y-1 px-6 py-2">
+                  <Link to="/prosthetic-services" className="block">Prosthetic Services</Link>
+                  <Link to="/orthotic-services" className="block">Orthotic Services</Link>
+                  <Link to="/diabetic-care" className="block">Diabetic Foot & Wound Care</Link>
+                  <Link to="/pediatric-care" className="block">Pediatric Care</Link>
+                </div>
+              )}
+            </div>
 
-            <button onClick={() => setMoreOpen(!moreOpen)} className="flex justify-between w-full px-4 py-2 bg-blue-500">
-              More <ChevronDown className="w-4 h-4" />
-            </button>
-            {moreOpen && (
-              <div className="bg-blue-100 text-gray-800 space-y-1 px-6 py-2">
-                <Link to="/gallery" onClick={() => setMobileOpen(false)}>Photo Gallery</Link>
-                <Link to="/facility" onClick={() => setMobileOpen(false)}>Our Facility</Link>
-              </div>
-            )}
+            {/* More Dropdown */}
+            <div>
+              <button
+                onClick={() => setMoreOpen(!moreOpen)}
+                className="w-full flex justify-between items-center px-4 py-2 bg-blue-500"
+              >
+                More <ChevronDown className="w-4 h-4" />
+              </button>
+              {moreOpen && (
+                <div className="bg-blue-100 text-gray-800 space-y-1 px-6 py-2">
+                  <Link to="/gallery" className="block">Photo Gallery</Link>
+                  <Link to="/facility" className="block">Our Facility</Link>
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
